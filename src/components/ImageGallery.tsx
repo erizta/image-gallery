a61034f12f8c0db4deb03cf2e7b9bbee3096f6eb
erizta <eriztaalifad@gmail.com>
2023-05-19 19:16:25 +0700

    show imagein image gallery

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,24 +1,28 @@
 import useFirestore from "../hooks/useFirestore";
 
 const ImageGallery = () => {
-  const { docs:images,isLoading } = useFirestore("images");
-if(isLoading){
-  return(
-    <div className="mt-10 text-center"><progress className="w-56 progress"></progress></div>
-  )
-}
-  
+  const { docs: images, isLoading } = useFirestore("images");
+  if (isLoading) {
+    return (
+      <div className="mt-10 text-center">
+        <progress className="w-56 progress"></progress>
+      </div>
+    );
+  }
+
   return (
     <div className="grid justify-center gap-4 mt-10 md:grid-cols-3">
-      <div className="shadow-xl card card-compact w-96 bg-base-100">
-        <figure>
-          <img src="" alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <p>Upload by:</p>
-          <span>Created at:</span>
+      {images.map((img, idx) => (
+        <div key={idx} className="w-full shadow-xl card card-compact bg-base-100">
+          <figure className="max-h-60">
+            <img src={img.image_url} alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <p>Upload by: {img.user_email}</p>
+            <span>Created at: {img.created_at.toLocaleDateString()}</span>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
